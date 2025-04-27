fc6e73b5f9a1571326767f8d876b042c1c26a77b
William Jackson <wcjaackson@gmail.com>
2025-04-27 22:50:35 +1000

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import PoliticalCompass from "@/components/PoliticalCompass/component";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50 p-4">
-      <PoliticalCompass />
-    </div>
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-gray-50 p-4">
+        <Button className="absolute top-2 left-2">Test</Button>
+        <PoliticalCompass />
+      </div>
+    </>
   );
 }
