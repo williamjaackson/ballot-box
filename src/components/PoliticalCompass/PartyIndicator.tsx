7ac11100cc5875bdf6844fc3661925be5856dde7
William Jackson <wcjaackson@gmail.com>
2025-04-28 21:37:06 +1000

    fix

--- src/components/PoliticalCompass/PartyIndicator.tsx
+++ src/components/PoliticalCompass/PartyIndicator.tsx
@@ -1,49 +1,38 @@
-function Label({
-  text,
-  color,
-}: {
-  text: string;
-  color: [number, number, number];
-}) {
+import { Party } from "@/context/PartiesContext";
+
+function Label({ party }: { party: Party }) {
   return (
     <p
-      className="text-white font-bold px-2 absolute text-md -translate-y-4/3 whitespace-nowrap transition-opacity group-hover:-translate-y-3/2 transition-transform duration-200"
+      className="pointer-events-none group-hover:-translate-y-1/4 transition-transform duration-200 text-white font-bold absolute text-md whitespace-nowrap transition-opacity left-1/2 -translate-x-1/2 px-2"
       style={{
-        backgroundColor: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
-        transform: "translateX(-.5rem)",
+        backgroundColor: `rgb(${party.color[0]}, ${party.color[1]}, ${party.color[2]})`,
+        transform: "translateY(-100%) translateY(-4px)",
       }}
     >
-      {text}
+      {party.alias}
     </p>
   );
 }
 
-export default function PartyIndicator({
-  party,
-}: {
-  party: {
-    name: string;
-    alias: string;
-    color: [number, number, number];
-    position: [number, number];
-  };
-}) {
+export default function PartyIndicator({ party }: { party: Party }) {
   // scale from -4, -4 to 4, 4
   const x = ((party.position[0] + 4) / 8) * 100;
   const y = ((-party.position[1] + 4) / 8) * 100;
 
   return (
     <div
-      className="absolute"
+      className="group absolute cursor-pointer"
       style={{
         left: `${x}%`,
         bottom: `${y}%`,
         transform: "translate(-50%, 50%)",
       }}
     >
+      {/* label */}
+      <Label party={party} />
       {/* border */}
       <div
-        className="rounded-full"
+        className="group-hover:scale-125 transition-transform duration-200 rounded-full"
         style={{
           border: `3px solid rgba(${party.color[0]}, ${party.color[1]}, ${party.color[2]}, 0.3)`,
         }}
