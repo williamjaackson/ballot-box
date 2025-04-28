2075ee3ad3dd14b211edab367609733f06f832f2
William Jackson <wcjaackson@gmail.com>
2025-04-28 14:40:24 +1000

    mobile responsiveness

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,43 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
-import { ChevronRight, Plus, Vote, X } from "lucide-react";
+import { ChevronRight, Vote, X } from "lucide-react";
 import Parties from "./Parties";
 
 export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
-  const sidebarWidth = "25rem";
+  const [sidebarWidth, setSidebarWidth] = useState("25rem");
+
+  // Check window width and add resize listener
+  useEffect(() => {
+    // Only run on client side
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setSidebarWidth(window.innerWidth < 768 ? "80vw" : "25rem");
+      };
+
+      // Set initial width
+      handleResize();
+
+      // Add resize listener
+      window.addEventListener("resize", handleResize);
+
+      // Cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []); // Empty dependency array since we're setting up the listener once
 
   return (
     <div className="flex">
+      {/* mobile overlay */}
       <div
-        className={`h-screen relative bg-white shadow-sm transition-all duration-300`}
+        className={`block md:hidden w-full h-full bg-black/50 absolute top-0 left-0 z-100 transition-all duration-300`}
+        style={{ opacity: isOpen ? 1 : 0 }}
+        onClick={() => setIsOpen(false)}
+      ></div>
+      <div
+        className={`h-screen z-100 absolute md:relative bg-white shadow-sm transition-all duration-300`}
         style={{ width: isOpen ? sidebarWidth : 0 }}
       >
         <div className="w-full overflow-hidden">
